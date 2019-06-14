cd62613a5ccf5ea37c08002588bc90590c004666
Bo9lPeH-Hub <Sudar.projeck.com>
2019-06-14 17:48:23 +0300

    My First Commit

--- utils/botinfo.js
+++ utils/botinfo.js
@@ -4,7 +4,7 @@ const GreenStyle = chalk.green; //Зелёный
 const GrayStyle = chalk.gray; // серый
 const config = require("./../config/bconfig.json");
 const cmd_control = require("./../config/cmdconfig.json");
-const os = require('os-utils');
+//const os = require('os-utils');
 
 module.exports.run = async (sdr, message, args) => {
   if(cmd_control.CMDbotinfo === false) return message.reply(cmd_control.Sbotinfo);
